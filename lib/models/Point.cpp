67386165727f261a8df445f41716dcf8f38ff4c0
xprss <vincenzo.sagristano@yahoo.com>
2025-11-29 17:07:30 +0100

    extended point on third dimension

--- lib/models/Point.cpp
+++ lib/models/Point.cpp
@@ -1,3 +1,3 @@
 #include "Point.hpp"
 
-Point::Point(double x, double y) : x_(x), y_(y) {}
+Point::Point(double x, double y, double z) : x_(x), y_(y), z_(z) {}
--- lib/models/Point.hpp
+++ lib/models/Point.hpp
@@ -3,15 +3,18 @@
 class Point
 {
 public:
-    Point(double x = 0.0, double y = 0.0);
+    Point(double x = 0.0, double y = 0.0, double z = 0.0);
 
     double getX() const { return x_; }
     double getY() const { return y_; }
+    double getZ() const { return z_; }
 
     void setX(double x) { x_ = x; }
     void setY(double y) { y_ = y; }
+    void setZ(double z) { z_ = z; }
 
 private:
     double x_;
     double y_;
+    double z_;
 };
